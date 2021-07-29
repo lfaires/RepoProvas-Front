612aebfd6f322c953fa2f0eb8a1d93a93a7e5c52
Luiz Fernando <lfaires@gmail.com>
2021-07-28 22:48:44 -0300

    home page layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,13 +9,23 @@ export default function Header(){
 }
 
 const Container = styled.header`
+    width: 100vw;
     height: 100px;
     background-color: #260B34;
     font-family: 'Ubuntu';
     font-size: 40px;
     color: #fff;
+    position: fixed;
+    top: 0;
+    left: 0;
     display: flex;
     align-items: center;
     padding-left: 35px;
     box-shadow: 0 1px 6px 0 rgb(64, 64, 64);
+
+    @media (max-width: 414px){
+        font-size: 28px;
+        justify-content: center;
+        padding-left: 0;
+    }
 `
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,54 @@
-import Header from './Header'
+import styled from 'styled-components';
+
+import Header from './Header';
 
 export default function HomePage() {
     return(
         <>
         <Header/>
+        <Body>
+            <Text>
+                This repository contains a set of past exams provided by students. 
+            </Text>
+            <br/>
+            <Text>
+                You can search for exams clicking on view button below and filter your search by instructor or course.
+            </Text>
+            <br/>
+            <Text>
+                If you want to include an exam into the repository click on send button bellow.
+            </Text>
+            <Form>
+                <Button>View Exams</Button>
+                <Button>Send Exam</Button>
+            </Form>
+        </Body>
         </>
     )
 }
+
+const Body = styled.main`
+    width: 950px;
+    margin-top: 130px;
+    margin-left: 35px;
+`
+const Text = styled.p`
+    font-family: 'Gowun Batang';
+    font-size: 20px;
+`
+const Form = styled.form`
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 50px;
+`
+const Button = styled.button`
+    background-color: #3C1053;
+    height: 50px;
+    width: 200px;
+    color: #fff;
+    margin-right: 30px;
+    border: none;
+
+`
